5c91746c3ad547e3ba43b08b2c09ea2db6ac207a
MelisaApriliani <melisaapriliani30@gmail.com>
2024-09-11 20:09:15 +0700

    remove previously created bckend project

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'; // Import the app created in app.ts
-
-const port = process.env.PORT || 3000; // Get port from environment variables or use default
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});

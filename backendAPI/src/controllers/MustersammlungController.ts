ee48c912936ed7053cccf510d737608b86c70fa1
MelisaApriliani <melisaapriliani30@gmail.com>
2024-09-15 05:49:35 +0700

    draw schein infromation of pdf template

--- backendAPI/src/controllers/MustersammlungController.ts
+++ backendAPI/src/controllers/MustersammlungController.ts
@@ -1,9 +1,11 @@
 import { AppDataSource } from '../ormconfig'; 
 import { Mustersammlung } from '../entity/schein/MustersammlungSchein';
+import { InsuranceProvider } from '../entity/InsuranceProvider';
 import { Request, Response } from 'express';
 import { PDFDocument, rgb } from 'pdf-lib';
 
 import fs from 'fs';
+import fontkit from 'fontkit';
 import path from 'path';
 
 export const createSchein = async (data: any) => {
@@ -17,17 +19,19 @@ export const generatePdf = async (scheinId: number) => {
     const schein = await scheinRepository.findOne({ 
         where: { id: Number(scheinId) }, 
         relations: ['type', 'patient', 'doctor', 'patientInsurance', 'healthcareFacility', 'hospitalTreatmentPerscriptionType', 'nearestRecommendedHospital']
-      });
-  
+    });
     if (!schein) {
         throw new Error('Schein not found');
     }
+    const insuranceProviderRepository = AppDataSource.getRepository(InsuranceProvider);
+    const insuranceProvider = await insuranceProviderRepository.findOneBy({ id: schein.patientInsurance?.id });
     
     try {
         // Load the PDF template
-        const templatePath = path.join(__dirname, '../../pdf_template/test1.pdf');
+        const templatePath = path.join(__dirname, '../../pdf_template/Mustersammlung.pdf');
         const templateBytes = fs.readFileSync(templatePath);
         const pdfDoc = await PDFDocument.load(templateBytes);
+        pdfDoc.registerFontkit(fontkit);
 
         // Flatten all form fields
         const form = pdfDoc.getForm();
@@ -49,7 +53,47 @@ export const generatePdf = async (scheinId: number) => {
 
        
         page.drawText(`${schein.patient.firstName}, ${schein.patient.lastName}`, {x: 28.5,  y: 228,  size, color: rgb(0, 0, 0),});
-      
+        page.drawText(`${schein.patient.dateOfBirth}`, {x:185,y:210,size,color: rgb(0, 0, 0),});
+        page.drawText(`${schein.patientInsurance?.insuranceNo}`, {x:108,y:181,size,color: rgb(0, 0, 0),});
+        page.drawText(`${schein.patientInsurance?.status}`, {x:203,y:181,size,color: rgb(0, 0, 0),});
+        page.drawText(`${schein.healthcareFacility.healthcareFacilityNumber}`, {x:28.5,y:155,size,color: rgb(0, 0, 0),});
+        page.drawText(`${schein.doctor.licenseNumber}`, {x:108,y:155,size,color: rgb(0, 0, 0),});
+        page.drawText(`${schein.date}`, {x:185,y:155,size,color: rgb(0, 0, 0),});
+        if(insuranceProvider){
+            console.log(`insurance provider:` + insuranceProvider.id+ ' '+insuranceProvider.insuranceIdentificationNumber);
+            page.drawText(`${insuranceProvider.id}`, {x:28.5,y:246,size,color: rgb(0, 0, 0),});
+            page.drawText(`${insuranceProvider.insuranceIdentificationNumber}`, {x:28.5,y:181,size,color: rgb(0, 0, 0),});
+        }
+        if(schein.nearestRecommendedHospital){
+            console.log(`nearest hospital:` + schein.nearestRecommendedHospital.name);
+            page.drawText(`${schein.nearestRecommendedHospital.name}`, {x:267,y:180,size,color: rgb(0, 0, 0),});
+        }
+        page.drawText(`${schein.diagnose}`, {x:28.5,y:116,size,color: rgb(0, 0, 0),});
+        const checkmark = '\u2714';
+
+        // Add custom font to support drawingcheckmark
+        const fontBytes = fs.readFileSync(path.join(__dirname, '../../font/DejaVuSans.ttf'));
+        const customFont = await pdfDoc.embedFont(fontBytes);
+        switch(schein.hospitalTreatmentPerscriptionType?.id){
+            case 5:
+                page.drawText(checkmark, {x: 265, y: 225,size,font:customFont, color: rgb(0, 0, 0),});
+                break;
+            case 6:
+                page.drawText(checkmark, {x: 265, y: 214,size,font:customFont, color: rgb(0, 0, 0),});
+                
+                break;
+            case 7:
+                page.drawText(checkmark, {x: 337, y: 225,size,font:customFont, color: rgb(0, 0, 0),});
+                break;
+            case 8:
+                page.drawText(checkmark, {x: 337, y: 214,size,font:customFont, color: rgb(0, 0, 0),});
+                break;
+            
+            default:
+                
+                break;
+        }
+
         const pdfBytes = await pdfDoc.save();
 
         console.log("PDF generated successfully", pdfBytes.length); 

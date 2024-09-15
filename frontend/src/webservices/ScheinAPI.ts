bfb40e40fbc7b898bf3423e67f7f938e1ea82dc5
MelisaApriliani <melisaapriliani30@gmail.com>
2024-09-15 19:13:34 +0700

    add module for all API calls

---
+++ frontend/src/webservices/ScheinAPI.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/api'; // Replace with your backend API URL
+
+export const ScheinAPI = {
+  getScheinTypes: async () => {
+    return axios.get(`${API_BASE_URL}/scheintype`);
+  },
+  getHealthcareFacilities: async () => {
+    return axios.get(`${API_BASE_URL}/healthcarefacilities`);
+  },
+  getDoctors: async () => {
+    return axios.get(`${API_BASE_URL}/doctors`);
+  },
+  getHospitalTreatmentTypes: async () => {
+    return axios.get(`${API_BASE_URL}/hospitaltreatmenttype`);
+  },
+  getInsuranceProviders: async () => {
+    return axios.get(`${API_BASE_URL}/insuranceprovider`);
+  },
+  createSchein: async (scheinData: any) => {
+    return axios.post(`${API_BASE_URL}/schein`, scheinData);
+  },
+  generatePdf: async (scheinTypeId: number, scheinId: number) => {
+    return axios.get(`${API_BASE_URL}/generate-pdf/${scheinTypeId}/${scheinId}`);
+  },
+};
+
+
